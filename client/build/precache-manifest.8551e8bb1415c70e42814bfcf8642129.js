477e223afacaa57bb3c107c969220ed1ad0980de
mtleinon <mika.t.leinonen@gmail.com>
2020-01-29 17:30:15 +0200

    Added client/build directory for AWS Elastic Beanstalk deployment.

---
+++ client/build/precache-manifest.8551e8bb1415c70e42814bfcf8642129.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719975d036e5cfb98a95fd4135c78440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f7333f2882ad35644db",
+    "url": "/static/css/main.9996185d.chunk.css"
+  },
+  {
+    "revision": "37316c82b5159876f11c",
+    "url": "/static/js/2.11f11ea5.chunk.js"
+  },
+  {
+    "revision": "374a015f9bd7dbb4f757a46f1c6a7946",
+    "url": "/static/js/2.11f11ea5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f7333f2882ad35644db",
+    "url": "/static/js/main.fb1c05d7.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "f988cd986fc1484fbb5d5c4a8fe7a8ed",
+    "url": "/static/media/info-circle.f988cd98.svg"
+  },
+  {
+    "revision": "be9054c0545cb3e22128f056e427f27d",
+    "url": "/static/media/window-close.be9054c0.svg"
+  }
+]);
